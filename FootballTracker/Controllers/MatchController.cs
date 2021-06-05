28fab079b46ca62cee0f124c9b0c0e149692efb7
akinniyThomas <akinniyithomas@yahoo.com>
2021-06-05 03:40:47 +0100

    match controller done

--- FootballTracker/Controllers/MatchController.cs
+++ FootballTracker/Controllers/MatchController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Models.Matches.Commands;
+using Application.Models.Matches.Queries;
+using Application.ViewModels;
+using Domain.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +15,26 @@ namespace FootballTracker.Controllers
     [Route("api/[controller]")]
     public class MatchController:ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public MatchController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<AnObjectResult<Match>> GetMatches() => await _mediator.Send(new GetMatchesQuery());
+
+        [HttpGet("{id}")]
+        public async Task<AnObjectResult<Match>> GetMatch(int id) => await _mediator.Send(new GetOneMatchQuery(id));
+
+        [HttpPost]
+        public async Task<AnObjectResult<Match>> AddMatch(Match match) => await _mediator.Send(new AddMatchCommand(match));
+
+        [HttpPut("{id}")]
+        public async Task<AnObjectResult<Match>> UpdateMatch(int id, Match match) => await _mediator.Send(new UpdateMatchCommand(id, match));
+
+        [HttpDelete("{id}")]
+        public async Task<AnObjectResult<Match>> DeleteMatch(int id) => await _mediator.Send(new DeleteMatchCommand(id));
     }
 }
